8e492438760338fb7334a1f5e2315ad9b2558a35
Denis Iavorskii <svirgibs@gmail.com>
2021-06-19 11:41:27 +0300

    Удаление неиспользуемых вспомогательных методов.

--- test_add_group.py
+++ test_add_group.py
@@ -14,7 +14,6 @@ class TestAddGroup(unittest.TestCase):
         self.wd.implicitly_wait(30)
     
     def test_add_group(self):
-        wd = self.wd
         self.login(wd, username="admin", password="secret")
         self.create_group(wd, Group(name="aasdasdasd", header="asdasdasd", footer="asdsad"))
         self.logout(wd)
@@ -26,12 +25,15 @@ class TestAddGroup(unittest.TestCase):
         self.logout(wd)
 
     def logout(self, wd):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
     def return_to_groups_page(self, wd):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
+        wd = self.wd
         self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
@@ -50,10 +52,12 @@ class TestAddGroup(unittest.TestCase):
         self.return_to_groups_page(wd)
 
     def open_groups_page(self, wd):
+        wd = self.wd
         # open groups page
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
+        wd = self.wd
         self.open_home_page(wd)
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -63,18 +67,9 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
+        wd = self.wd
         # open home page
         wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
     
     def tearDown(self):
         self.wd.quit()
