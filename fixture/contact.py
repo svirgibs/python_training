e72dac830c6b6bfe4d60c8959b0be24d44d035b8
Denis Iavorskii <svirgibs@gmail.com>
2021-06-19 14:54:12 +0300

    Задание №5. Добавление нового помощника contact.py Перенос файлов в необходимые папки (пакеты)

---
+++ fixture/contact.py
@@ -0,0 +1,32 @@
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create_new_contact(self, contact):
+        wd = self.app.wd
+        self.init_add_new_contact()
+        # fill form
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        self.return_to_home_page()
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def init_add_new_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
