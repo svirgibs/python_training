245b71e18f347beea47c410941016ce2ffc85bea
Denis Iavorskii <svirgibs@gmail.com>
2021-06-19 15:24:19 +0300

    Задание №6. Создание общей фикстуры для всех тестов. Добавление нового теста "Удаление первой группы". Рефакторинг.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,13 +4,6 @@ from model.contact import Contact
 from fixture.application import Application
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create_new_contact(Contact(firstname="Denis", middlename="Timurovich", lastname="Iavorskii",
