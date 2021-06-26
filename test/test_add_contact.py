b25d7894e6be78bd51732ea4a63c56f9e9d409e6
Denis Iavorskii <svirgibs@gmail.com>
2021-06-26 23:00:25 +0300

    Интеллектуальная фикстура.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,5 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_new_contact(Contact(firstname="Denis", middlename="Timurovich", lastname="Iavorskii",
                                             address="Moscow", mobile="+79167774455", email="yavorskiy.dt@gmail.com"))
-    app.session.logout()
--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -2,7 +2,5 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="NewDenis", middlename="NewTimurovich", lastname="NewIavorskii",
                                             address="Moscow", mobile="+79167774455", email="yavorskiy.dt@gmail.com"))
-    app.session.logout()
