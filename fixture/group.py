4067a6ebc19c9a7f432f7bb7fc18c402813dc1fb
Denis Iavorskii <svirgibs@gmail.com>
2021-06-26 22:07:01 +0300

    Оптимизация ввода данных в формы.

--- fixture/group.py
+++ fixture/group.py
@@ -21,33 +21,40 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
-    def mod_first_group(self, group):
+    def select_first_group(self):
+        wd = self.app.wd
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # init modification
         wd.find_element_by_name("edit").click()
         # fill new data
-        self.fill_group_form(group)
+        self.fill_group_form(new_group_data)
         # submit update
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def fill_group_form(self, group):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def open_groups_page(self):
         wd = self.app.wd
--- test/test_mod_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_modify_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.mod_first_group(Group(name="new abc", header="new qwe", footer="new asd"))
-    app.session.logout()
