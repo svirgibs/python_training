390fba9d3bf4b3b2f30894597c80116780038b36
Denis Yavorskiy <svirgibs@gmail.com>
2021-07-14 13:03:04 +0300

    Задание №15: Параметризовать тесты для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname=random_string("firstname", 8), middlename=random_string("middlename", 8),
+                    lastname=random_string("lastname", 12), address=random_string("address", 20),
+                    mobile=random_string("mobile", 11), email=random_string("email", 12))
+            for i in range(5)]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Denis", middlename="Timurovich", lastname="Iavorskii",
-                                            address="Moscow", mobile="+79167774455", email="yavorskiy.dt@gmail.com")
     app.contact.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
