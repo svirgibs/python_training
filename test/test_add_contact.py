bbc16705c0c335179c21398c7085bb450049a8a0
Denis Yavorskiy <svirgibs@gmail.com>
2021-07-15 11:31:08 +0300

    Удаление точек и спец символов из рандомных строчек при создании нового контакта и группы.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,7 +6,7 @@ import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
