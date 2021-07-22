41d4e55c3804d8c7f5a2e5e2d42e35a9823b5ecc
Denis Yavorskiy <svirgibs@gmail.com>
2021-07-22 22:36:52 +0300

    Задание №18: Реализовать генераторы тестовых данных

---
+++ generator/contact.py
@@ -0,0 +1,51 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_numbers(prefix):
+    symbols = string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(9)])
+
+
+def random_email(postfix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + postfix
+
+
+testdata = [Contact(firstname=random_string("firstname", 8), middlename=random_string("middlename", 8),
+                    lastname=random_string("lastname", 12), address=random_string("address", 20),
+                    mobile=random_numbers("+79"), email=random_email("@gmail.com", 10))
+            for i in range(n)]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname=random_string("firstname", 8), middlename=random_string("middlename", 8),
-                    lastname=random_string("lastname", 12), address=random_string("address", 20),
-                    mobile=random_string("mobile", 11), email=random_string("email", 12))
-            for i in range(5)]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
