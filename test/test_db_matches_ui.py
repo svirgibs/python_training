bbc403241e50112ce6f28cc9990e41eeb5e4dc90
Denis Yavorskiy <svirgibs@gmail.com>
2021-07-27 18:56:20 +0300

    Переделываем тесты: проверяем данные, загруженные из базы данных

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,9 +1,12 @@
 from model.group import Group
+from timeit import timeit
 
 
 def test_group_list(app, db):
     ui_list = app.group.get_group_list()
+    print("\n" + str(timeit(lambda: app.group.get_group_list(), number=1)))
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
-    assert  sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+    print("\n" + str(timeit(lambda: map(clean, db.get_group_list()), number=1000)))
+    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
